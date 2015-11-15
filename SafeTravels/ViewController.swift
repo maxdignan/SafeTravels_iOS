7b9b973615915352854b77b35144e4fd77fd6aa1
maxdignan <maxdignan@gmail.com>
2015-11-14 21:48:22 -0500

    init

---
+++ SafeTravels/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SafeTravels
+//
+//  Created by Max Dignan on 11/14/15.
+//  Copyright © 2015 Dignan. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
